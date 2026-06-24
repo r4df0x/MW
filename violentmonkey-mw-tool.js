12ccb95cd340602ffcf54c74435df101a9e70b40
r4df0x <57815348+r4df0x@users.noreply.github.com>
2026-06-24 17:08:02 +0300

    Change match URL for UserScript
    Updated the match pattern to be more specific for the script.

--- violentmonkey-mw-tool.js
+++ violentmonkey-mw-tool.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Load GoMining-Miner-Wars-Tool & Align Left
-// @match        https://app.gomining.com/*
+// @match        https://app.gomining.com/miner-wars/mining
 // @run-at       document-idle
 // @grant        none
 // @inject-into  page
